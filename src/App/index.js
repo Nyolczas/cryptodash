2130a1571d12d6a5cdaecf7e9746f8acc4e1764d
Istvan Nyolczas <31048051+Nyolczas@users.noreply.github.com>
2020-10-23 12:46:51 +0200

    font seetup

--- src/App/index.js
+++ src/App/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import './App.css';
+import styled, { css } from 'styled-components';
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        Hello world
+        <h1>Welcome</h1>
       </div>
-    )
+    );
   }
 }
 
