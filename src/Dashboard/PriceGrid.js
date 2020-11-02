e612494b1f81bde8db566de3d699d55c53e0a4eb
Istvan Nyolczas <31048051+Nyolczas@users.noreply.github.com>
2020-11-02 08:12:51 +0100

    price grid, price tile

--- src/Dashboard/PriceGrid.js
+++ src/Dashboard/PriceGrid.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
 import { AppContext } from '../App/AppProvider';
+import PriceTile from './PriceTile';
 
 const PriceGrid = styled.div`
   display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  grid-gap: 1rem;
 `;
 
 export default () => {
@@ -11,8 +14,8 @@ export default () => {
     <AppContext.Consumer>
       {({ prices }) => (
         <PriceGrid>
-          {prices.map((price) => (
-            <div> {Object.keys(price)[0]} </div>
+          {prices.map((price, index) => (
+            <PriceTile key={index} index={index} price={price} />
           ))}
         </PriceGrid>
       )}
