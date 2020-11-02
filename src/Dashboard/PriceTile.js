75de7fb60f38fd885f718602d94eb20bea8a912c
Istvan Nyolczas <31048051+Nyolczas@users.noreply.github.com>
2020-11-02 21:06:21 +0100

    Dashboard: update

--- src/Dashboard/PriceTile.js
+++ src/Dashboard/PriceTile.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
 import { SelectableTile } from '../Shared/Tile';
-import { fontSizeSm } from '../Shared/Styles';
+import { fontSizeSm, fontSizeLg } from '../Shared/Styles';
 
 const PriceTileStyled = styled(SelectableTile)`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 0.5rem;
   ${(props) =>
     props.compact &&
     css`
@@ -11,14 +14,39 @@ const PriceTileStyled = styled(SelectableTile)`
     `}
 `;
 
+const JustifyRight = styled.div`
+  justify-self: right;
+`;
+
+const ChangePct = styled.div`
+  color: ${(props) => (props.pct > 0 ? 'greenYellow' : 'red')};
+`;
+
+const TickerPrice = styled.div`
+  grid-column: 1 / span 2;
+  ${fontSizeLg}
+`;
+
+const numberFormat = (number) => {
+  return number.toFixed(2);
+};
+
+const PriceTile = ({ sym, data }) => {
+  return (
+    <PriceTileStyled>
+      <div> {sym} </div>
+      <JustifyRight>
+        <ChangePct pct={data.CHANGEPCT24HOUR}>
+          {numberFormat(data.CHANGEPCT24HOUR)}
+        </ChangePct>
+      </JustifyRight>
+      <TickerPrice> $ {numberFormat(data.PRICE)} </TickerPrice>
+    </PriceTileStyled>
+  );
+};
+
 export default ({ price, index }) => {
   let sym = Object.keys(price)[0];
   let data = price[sym]['USD'];
-
-  return (
-    <PriceTileStyled compact={index >= 5}>
-      {sym}
-      {data.PRICE}
-    </PriceTileStyled>
-  );
+  return <PriceTile sym={sym} data={data}></PriceTile>;
 };
