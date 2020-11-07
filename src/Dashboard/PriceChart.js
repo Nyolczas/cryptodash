1dbcdb64adfc313d664e5b7b74e52b2c5cf18246
Istvan Nyolczas <31048051+Nyolczas@users.noreply.github.com>
2020-11-07 18:12:05 +0100

    HighCharts Theme

--- src/Dashboard/PriceChart.js
+++ src/Dashboard/PriceChart.js
@@ -3,11 +3,13 @@ import React from 'react';
 import { Tile } from '../Shared/Tile';
 import { AppContext } from '../App/AppProvider';
 import ReactHighcharts from 'react-highcharts';
+import HighchartsTheme from './HighchartsTheme';
+ReactHighcharts.Highcharts.setOptions(HighchartsTheme);
 
 export default () => {
   return (
     <AppContext.Consumer>
-      {({}) => (
+      {() => (
         <Tile>
           <ReactHighcharts config={HighchartsConfig()} />
         </Tile>
